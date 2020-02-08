c81d4c6d522dbe778c47a9216f643ca13dfaa3c6
Daniel Zikmund <danielzikmund@live.com>
2020-02-08 10:56:04 +0100

    Partially implemented video on main menu, needs resizing and repositioning

--- src/Topics/Physics.HomogenousMovement/Views/MainMenuView.xaml.cs
+++ src/Topics/Physics.HomogenousMovement/Views/MainMenuView.xaml.cs
@@ -18,9 +18,15 @@ namespace Physics.HomogenousMovement.Views
         {
             this.InitializeComponent();
             VideoBackground.MediaPlayer.MediaOpened += MediaPlayer_MediaOpened;
+            VideoBackground.MediaPlayer.IsLoopingEnabled = true;
             DataContextChanged += MainMenuView_DataContextChanged;
             RootGrid.Opacity = 0;
             RootGrid.OpacityTransition = new Windows.UI.Xaml.ScalarTransition()
+            {
+                Duration = TimeSpan.FromSeconds(0.3)
+            };
+
+            Splash.OpacityTransition = new Windows.UI.Xaml.ScalarTransition()
             {
                 Duration = TimeSpan.FromSeconds(0.3)
             };
@@ -29,8 +35,11 @@ namespace Physics.HomogenousMovement.Views
         private async void MediaPlayer_MediaOpened(Windows.Media.Playback.MediaPlayer sender, object args)
         {
             await this.Dispatcher.RunAsync(
-                Windows.UI.Core.CoreDispatcherPriority.Normal, 
-                () => RootGrid.Opacity = 1);
+                Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    RootGrid.Opacity = 1;
+                    Splash.Opacity = 0;
+                });
         }
 
         public MainMenuViewModel Model { get; private set; }
