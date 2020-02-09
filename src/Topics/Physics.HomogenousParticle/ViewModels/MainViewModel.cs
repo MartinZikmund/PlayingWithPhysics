fa4d01a52973fd99fc0eb4c51b164d3bc2712b0d
Daniel Zikmund <danielzikmund@live.com>
2020-02-09 13:05:28 +0100

    Implement UI inputs and bindings Restrict input values Implement draw button with command bindings

--- src/Topics/Physics.HomogenousParticle/ViewModels/MainViewModel.cs
+++ src/Topics/Physics.HomogenousParticle/ViewModels/MainViewModel.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml;
 
 namespace Physics.HomogenousParticle.ViewModels
 {
@@ -16,11 +18,58 @@ namespace Physics.HomogenousParticle.ViewModels
 
         public MainViewModel()
         {
+            OnSelectedVariantIndexChanged();
         }
 
         public override void Prepare(NavigationModel parameter)
         {
         }
-        public VelocityVariant Variant { get; set; }
+        public int SelectedVariantIndex { get; set; }
+
+        public bool IsQEnabled { get; set; }
+        public bool IsBEnabled { get; set; }
+        public bool IsVEnabled { get; set; }
+
+        public void OnSelectedVariantIndexChanged()
+        {
+            DisableInputs();
+        }
+
+        private void DisableInputs()
+        {
+            switch ((VelocityVariant)SelectedVariantIndex)
+            {
+                case VelocityVariant.Zero:
+                    IsQEnabled = true;
+                    IsBEnabled = true;
+                    IsVEnabled = false;
+                    break;
+                case VelocityVariant.Parallel:
+                    IsQEnabled = true;
+                    IsBEnabled = true;
+                    IsVEnabled = true;
+                    break;
+                case VelocityVariant.Perpendicular:
+                    IsQEnabled = true;
+                    IsBEnabled = true;
+                    IsVEnabled = true;
+                    break;
+            }
+        }
+
+        public float QInput { get; set; }
+        public float OrientationInput { get; set; }
+        public float VelocityInput { get; set; }
+
+        public ICommand DrawCommand => GetOrCreateCommand(DrawMotion);
+
+        public void DrawMotion()
+        {
+            DrawingContent = "Drawing...";
+        }
+
+        public string DrawingContent { get; set; }
+
+        public Visibility IsSecondStepVisible { get; set; } = Visibility.Visible;
     }
 }
