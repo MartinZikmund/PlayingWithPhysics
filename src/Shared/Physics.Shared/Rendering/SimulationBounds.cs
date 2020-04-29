a6db0372ebecaa1f33a0541ff19c03cbf7a29a70
Martin Zikmund <martin@zikmund.dev>
2020-04-29 18:02:23 +0200

    Implemented arrows!

---
+++ src/Shared/Physics.Shared/Rendering/SimulationBounds.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Physics.Shared.Rendering
+{
+    public struct SimulationBounds
+    {
+        /// <summary>
+        /// Top - Y = "0"
+        /// Left - X = "0"
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="top"></param>
+        /// <param name="right"></param>
+        /// <param name="bottom"></param>
+        public SimulationBounds(float left, float top, float right, float bottom)
+        {
+            Left = left;
+            Bottom = bottom;
+            Right = right;
+            Top = top;
+        }
+
+        public float Left { get; }
+
+        public float Bottom { get; }
+
+        public float Right { get; }
+
+        public float Top { get; }
+    }
+}
--- src/Topics/Physics.HomogenousParticle/Rendering/HomogenousParticleCanvasController.cs
+++ src/Topics/Physics.HomogenousParticle/Rendering/HomogenousParticleCanvasController.cs
@@ -1,4 +1,5 @@
 ﻿using MathNet.Spatial.Euclidean;
+using Microsoft.Graphics.Canvas.Geometry;
 using Microsoft.Graphics.Canvas.UI.Xaml;
 using Physics.Shared.Helpers;
 using Physics.Shared.Rendering;
@@ -16,12 +17,12 @@ namespace Physics.HomogenousParticle.Rendering
 {
     public abstract class HomogenousParticleCanvasControllerBase : BaseCanvasController, IDisposable
     {
-        private const int ArrowDistance = 20;
+        private const int ArrowDistance = 80;
 
         protected int SimulationPadding { get; set; } = 52;
 
-        protected RectangleF _simulationBoundsInMeters = new RectangleF();
-        private RectangleF _simulationBoundsInPixels = new RectangleF();
+        protected SimulationBounds _simulationBoundsInMeters = new SimulationBounds();
+        private SimulationBounds _simulationBoundsInPixels = new SimulationBounds();
 
         public HomogenousParticleCanvasControllerBase(CanvasAnimatedControl canvasAnimatedControl) : base(canvasAnimatedControl)
         {
@@ -29,25 +30,25 @@ namespace Physics.HomogenousParticle.Rendering
 
         public override void Update(ICanvasAnimatedControl sender)
         {
-            _simulationBoundsInPixels = new RectangleF(
+            _simulationBoundsInPixels = new SimulationBounds(
                 SimulationPadding,
                 SimulationPadding,
-                (float)sender.Size.Width - SimulationPadding * 2,
-                (float)sender.Size.Height - SimulationPadding * 2);
+                (float)sender.Size.Width - SimulationPadding,
+                (float)sender.Size.Height - SimulationPadding);
         }
 
         protected void DrawInductionArrows(double angle, Windows.UI.Color color, CanvasAnimatedDrawEventArgs args)
         {
             // calculate perpendicular line through the simulation center
             var center = new Point2D(_canvasAnimatedControl.Size.Width / 2f, _canvasAnimatedControl.Size.Height / 2f);
-            var perpendicularAngle = angle + 90;
+            var perpendicularAngle = angle - 90;
             var perpendicularLine = CalculatePointLineUnderAngle(perpendicularAngle, center);
             var normalized = perpendicularLine.Direction;
-            int i = 1;
+            int i = 0;
             while (i <= 10)
             {
                 var upX = center.X + normalized.X * i * ArrowDistance;
-                var upY = center.X + normalized.Y * i * ArrowDistance;
+                var upY = center.Y + normalized.Y * i * ArrowDistance;               
 
                 var upLine = CalculatePointLineUnderAngle(angle, new Point2D(upX, upY));
                 DrawInductionArrow(upLine, angle, color, args);
@@ -58,7 +59,7 @@ namespace Physics.HomogenousParticle.Rendering
                 var inverseLine = CalculatePointLineUnderAngle(angle, new Point2D(inverseX, inverseY));
                 DrawInductionArrow(inverseLine, angle, color, args);
 
-                i++;           
+                i++;
             }
             //for (int i = 1; i < ArrowCount; i++)
             //{
@@ -82,15 +83,114 @@ namespace Physics.HomogenousParticle.Rendering
 
         private void DrawInductionArrow(Line2D line, double angle, Color color, CanvasAnimatedDrawEventArgs args)
         {
-            var intersectionBottom = line.IntersectWith(new Line2D(new Point2D(SimulationPadding, SimulationPadding), new Point2D(SimulationPadding + 1, SimulationPadding)));
-            var intersectionTop = line.IntersectWith(new Line2D(new Point2D(0, _simulationBoundsInPixels.Bottom), new Point2D(1, _simulationBoundsInPixels.Bottom)));
-            if (intersectionBottom != null && intersectionTop != null)
-                args.DrawingSession.DrawLine(new Vector2((float)intersectionBottom.Value.X, (float)intersectionBottom.Value.Y), new Vector2((float)intersectionTop.Value.X, (float)intersectionTop.Value.Y), color);
+            angle = ((angle % 360.0) + 360) % 360.0;
+            var touchPoints = new List<Point2D>();
+
+            var intersectionBottom = line.IntersectWith(new Line2D(new Point2D(_simulationBoundsInPixels.Left, _simulationBoundsInPixels.Bottom), new Point2D(_simulationBoundsInPixels.Left + 1, _simulationBoundsInPixels.Bottom)));
+            if (intersectionBottom != null && _simulationBoundsInPixels.Left <= intersectionBottom.Value.X && intersectionBottom.Value.X <= _simulationBoundsInPixels.Right)
+            {
+                touchPoints.Add(intersectionBottom.Value);
+            }
+
+            var intersectionTop = line.IntersectWith(new Line2D(new Point2D(_simulationBoundsInPixels.Left, _simulationBoundsInPixels.Top), new Point2D(_simulationBoundsInPixels.Left + 1, _simulationBoundsInPixels.Top)));
+            if (intersectionTop != null && _simulationBoundsInPixels.Left <= intersectionTop.Value.X && intersectionTop.Value.X <= _simulationBoundsInPixels.Right)
+            {
+                touchPoints.Add(intersectionTop.Value);
+            }
+
+            var intersectionLeft = line.IntersectWith(new Line2D(new Point2D(_simulationBoundsInPixels.Left, _simulationBoundsInPixels.Bottom), new Point2D(_simulationBoundsInPixels.Left, _simulationBoundsInPixels.Bottom + 1)));
+            if (intersectionLeft != null && intersectionLeft.Value.Y != _simulationBoundsInPixels.Bottom && intersectionLeft.Value.Y != _simulationBoundsInPixels.Top &&
+                 _simulationBoundsInPixels.Bottom >= intersectionLeft.Value.Y && intersectionLeft.Value.Y >= _simulationBoundsInPixels.Top)
+            {
+                touchPoints.Add(intersectionLeft.Value);
+            }
+            var intersectionRight = line.IntersectWith(new Line2D(new Point2D(_simulationBoundsInPixels.Right, _simulationBoundsInPixels.Bottom), new Point2D(_simulationBoundsInPixels.Right, _simulationBoundsInPixels.Bottom + 1)));
+            if (intersectionRight != null && intersectionRight.Value.Y != _simulationBoundsInMeters.Bottom && intersectionRight.Value.Y != _simulationBoundsInPixels.Top &&
+                 _simulationBoundsInPixels.Bottom >= intersectionRight.Value.Y && intersectionRight.Value.Y >= _simulationBoundsInPixels.Top)
+            {
+                touchPoints.Add(intersectionRight.Value);
+            }
+
+            if (touchPoints.Count == 2)
+            {
+                var firstPoint = new Vector2((float)touchPoints[0].X, (float)touchPoints[0].Y);
+                var secondPoint = new Vector2((float)touchPoints[1].X, (float)touchPoints[1].Y);
+                args.DrawingSession.DrawLine(firstPoint, secondPoint, color, 2f);
+
+                Vector2 arrowPoint;
+                if (angle > 270 || angle < 90)
+                {
+                    // the arrow is pointing to the "right"
+                    if (firstPoint.X > secondPoint.X)
+                    {
+                        arrowPoint = firstPoint;
+                    }
+                    else
+                    {
+                        arrowPoint = secondPoint;
+                    }
+                }
+                else if (angle > 90 && angle < 270)
+                {
+                    if (firstPoint.X < secondPoint.X)
+                    {
+                        arrowPoint = firstPoint;
+                    }
+                    else
+                    {
+                        arrowPoint = secondPoint;
+                    }
+                }
+                else if (angle == 90)
+                {
+                    if (firstPoint.Y < secondPoint.Y)
+                    {
+                        arrowPoint = firstPoint;
+                    }
+                    else
+                    {
+                        arrowPoint = secondPoint;
+                    }
+                }
+                else
+                {
+                    if (firstPoint.Y > secondPoint.Y)
+                    {
+                        arrowPoint = firstPoint;
+                    }
+                    else
+                    {
+                        arrowPoint = secondPoint;
+                    }
+                }
+
+                DrawArrowTip(arrowPoint, angle, color, args);
+            }            
+        }
+
+        private void DrawArrowTip(Vector2 point, double angle, Color color, CanvasAnimatedDrawEventArgs args)
+        {
+            // Calculate unit vector of the given angle
+            var directionX = Math.Cos(MathHelpers.DegreesToRadians((float)angle));
+            var directionY = -Math.Sin(MathHelpers.DegreesToRadians((float)angle));
+            Vector2 v = new Vector2((float)directionX, (float)directionY);
+            // Invert it
+            v = -v;
+            // Move point back the given distance
+            var pointMovedBack = point + v * 10;
+            // Calculate perpendicular vector
+            var vectorPerpendicular = new Vector2(v.Y, -v.X);
+            // calculate head ends
+            var firstPoint = pointMovedBack + vectorPerpendicular * 5;
+            var secondPoint = pointMovedBack - vectorPerpendicular * 5;
+            // draw arrow lines
+            args.DrawingSession.DrawLine(firstPoint, point, color, 2f);
+            args.DrawingSession.DrawLine(secondPoint, point, color, 2f);
         }
 
         private Line2D CalculatePointLineUnderAngle(double angle, Point2D point)
         {
-            var a = Math.Tan(MathHelpers.DegreesToRadians((float)angle));
+            var a = Math.Tan(MathHelpers.DegreesToRadians(-(float)angle));
             var b = point.Y - a * point.X;
 
             var x1 = point.X + 1;
