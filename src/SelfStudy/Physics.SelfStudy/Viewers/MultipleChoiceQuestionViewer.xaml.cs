64a1c95e04654d55b75381a1f655de1382fb76e4
Martin Zikmund <martin@zikmund.dev>
2020-04-30 18:00:57 +0200

    Continued work on editor

---
+++ src/SelfStudy/Physics.SelfStudy/Viewers/MultipleChoiceQuestionViewer.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Physics.SelfStudy.Models.Contents.Abstract;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Physics.SelfStudy.Viewers
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MultipleChoiceQuestionViewer : Page
+    {
+        public MultipleChoiceQuestionViewer()
+        {
+            this.InitializeComponent();
+        }
+
+        public MultipleChoiceQuestionContent Question
+        {
+            get { return (MultipleChoiceQuestionContent)GetValue(QuestionProperty); }
+            set { SetValue(QuestionProperty, value); }
+        }
+
+        public static readonly DependencyProperty QuestionProperty =
+            DependencyProperty.Register(nameof(Question), typeof(MultipleChoiceQuestionContent), typeof(MultipleChoiceQuestionViewer), new PropertyMetadata(null, OnQuestionChanged));
+
+        public MultipleChoiceQuestionViewerViewModel Model
+        {
+            get { return (MultipleChoiceQuestionViewerViewModel)GetValue(ModelProperty); }
+            set { SetValue(ModelProperty, value); }
+        }
+
+        public static readonly DependencyProperty ModelProperty =
+            DependencyProperty.Register(nameof(Model), typeof(MultipleChoiceQuestionViewerViewModel), typeof(MultipleChoiceQuestionViewer), new PropertyMetadata(0));
+
+
+
+        private static void OnQuestionChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var viewer = (MultipleChoiceQuestionViewer)d;
+            if (e.NewValue is MultipleChoiceQuestionContent question)
+            {
+                viewer.Model = new MultipleChoiceQuestionViewerViewModel(question);   
+            }
+        }
+    }
+}
---
+++ src/SelfStudy/Physics.SelfStudy/Viewers/MultipleChoiceQuestionViewerViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Physics.SelfStudy.Editor.ViewModels;
+using Physics.SelfStudy.Models.Contents.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Physics.SelfStudy.Viewers
+{
+    public class MultipleChoiceQuestionViewerViewModel : ViewModelBase
+    {
+        private readonly MultipleChoiceQuestionContent _question;
+
+        public MultipleChoiceQuestionViewerViewModel(MultipleChoiceQuestionContent question)
+        {
+            _question = question;
+        }
+
+        public int SelectedAnswerIndex { get; set; }
+
+        public bool ShowQuestion { get; set; } = true;
+
+        public bool ShowCorrectAnswer { get; set; }
+
+        public bool ShowWrongAnswer { get; set; }
+
+        public ICommand RevealAnswerCommand => GetOrCreateCommand(RevealAnswer);
+
+        private void RevealAnswer()
+        {
+            ShowQuestion = false;
+            if (SelectedAnswerIndex == _question.CorrectOptionIndex)
+            {
+                ShowCorrectAnswer = true;
+            }
+            else
+            {
+                ShowWrongAnswer = true;
+            }
+        }
+    }
+}
