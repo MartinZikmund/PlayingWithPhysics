0d2e6c2f60e1c2f41b94eef18b4596be6471b99a
DZetko <danielzikmund@live.com>
2020-07-30 11:29:22 +0200

    Připomínky 23_7 a,b

--- src/Topics/Physics.HomogenousParticle/ViewModels/State/ParallelVariantStateViewModel.cs
+++ src/Topics/Physics.HomogenousParticle/ViewModels/State/ParallelVariantStateViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Physics.HomogenousParticle.Services;
+using Physics.HomongenousParticle.Logic.PhysicsServices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +10,14 @@ namespace Physics.HomogenousParticle.ViewModels.State
 {
     public class ParallelVariantStateViewModel : VariantStateViewModelBase
     {
+        private readonly ParallelPhysicsService _physicsService;
+        private readonly ParallelMotionSetup _motionSetup;
         public ParallelVariantStateViewModel(IMotionSetup motionSetup) : base(motionSetup)
         {
+            _motionSetup = (ParallelMotionSetup)motionSetup;
+            _physicsService = new ParallelPhysicsService(_motionSetup);   
         }
+
+        public string Multiple => _motionSetup.Velocity.ToString();
     }
 }
