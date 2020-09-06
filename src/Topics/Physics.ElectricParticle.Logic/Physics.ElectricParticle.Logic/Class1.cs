daa3664c6fa350c004832b219cb20fb133c921a9
DZetko <danielzikmund@live.com>
2020-09-06 17:33:59 +0200

    Create App#13 project WIP: Basic setup

---
+++ src/Topics/Physics.ElectricParticle.Logic/Physics.ElectricParticle.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Physics.ElectricParticle.Logic
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Topics/Physics.ElectricParticle/Physics.ElectricParticle/Dialogs/AddOrUpdateMovement.xaml.cs
@@ -0,0 +1,51 @@
+﻿using DynamicData.Annotations;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Physics.ElectricParticle.Dialogs
+{
+    public sealed partial class AddOrUpdateMovement : ContentDialog
+    {
+        public AddOrUpdateMovement()
+        {
+            this.InitializeComponent();
+        }
+        public AddOrUpdateMovementViewModel Model { get; private set; }
+        private void SetupNumberBoxFormattings()
+        {
+            //StartXNumberBox.SetupFormatting(fractionDigits: 2);
+            //StartYNumberBox.SetupFormatting(fractionDigits: 2);
+            //GravityNumberBox.SetupFormatting(fractionDigits: 2);
+            //MassNumberBox.SetupFormatting(fractionDigits: 2);
+            //V0NumberBox.SetupFormatting(fractionDigits: 2);
+            //AngleNumberBox.SetupFormatting(fractionDigits: 2);
+            //GravityNumberBox.SmallChange = 0.1;
+        }
+
+        private async void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            var deferral = args.GetDeferral();
+            Setup = await Model.CreateMotionSetupAsync();
+            if (Setup == null)
+            {
+                args.Cancel = true;
+            }
+            deferral.Complete();
+        }
+
+        public IMotionSetup Setup { get; set; }
+    }
+}
