bd19248deb4b2e6983d9f631b62abb535e9618b3
Martin Zikmund <martin@zikmund.dev>
2020-12-30 13:48:41 +0100

    Mass description switching

---
+++ src/Shared/Physics.Shared/Converters/ValueMatchVisibilityConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Physics.Shared.UI.Converters
+{
+	public class ValueMatchVisibilityConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			var areEqual = parameter.Equals(value?.ToString());
+			return areEqual ? Visibility.Visible : Visibility.Collapsed;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language) => throw new NotImplementedException();
+	}
+}
