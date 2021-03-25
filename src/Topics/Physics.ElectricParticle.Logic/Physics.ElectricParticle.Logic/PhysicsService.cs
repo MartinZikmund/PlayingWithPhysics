2a9f9f3890c3fdf9a6baf9f1bbff2c8cace616a8
Martin Zikmund <martin@zikmund.dev>
2021-03-25 08:28:13 +0100

    Oprava delta T

--- src/Topics/Physics.ElectricParticle.Logic/Physics.ElectricParticle.Logic/PhysicsService.cs
+++ src/Topics/Physics.ElectricParticle.Logic/Physics.ElectricParticle.Logic/PhysicsService.cs
@@ -349,6 +349,10 @@ namespace Physics.ElectricParticle.Logic
 		public BigNumber ComputeDeltaT(int totalFrames)
 		{
 			var acceleration = ComputeA();
+			if (acceleration.Mantisa < 0)
+			{
+				acceleration = new BigNumber(-acceleration.Mantisa, acceleration.Exponent);
+			}
 			BigNumber top = -_setup.Particle.StartVelocity + (_setup.Particle.StartVelocity * _setup.Particle.StartVelocity + acceleration).Sqrt();
 			BigNumber bottom = acceleration * totalFrames;
 			return top / bottom;
