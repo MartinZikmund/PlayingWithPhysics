ca0be4210f519f0d8089da87b54fc5fc40faeff3
Martin Zikmund <martin@zikmund.dev>
2021-05-20 11:36:54 +0200

    In progress game

--- src/Topics/Physics.ElectricParticle/Physics.ElectricParticle/Rendering/GameCanvasController.cs
+++ src/Topics/Physics.ElectricParticle/Physics.ElectricParticle/Rendering/GameCanvasController.cs
@@ -5,12 +5,12 @@ namespace Physics.ElectricParticle.Rendering
 {
 	public class GameCanvasController : SkiaCanvasController
 	{
-		public GameCanvasController(ISkiaCanvas control):base(control)
+		public GameCanvasController(ISkiaCanvas control) : base(control)
 		{
 
 		}
 
-		public override void Draw(ISkiaCanvas sender, SKSurface args) => throw new System.NotImplementedException();
-		public override void Update(ISkiaCanvas sender) => throw new System.NotImplementedException();
+		public override void Draw(ISkiaCanvas sender, SKSurface args) { }
+		public override void Update(ISkiaCanvas sender) { }
 	}
 }
--- src/Topics/Physics.ElectricParticle/Physics.ElectricParticle/ViewModels/GameViewModel.cs
+++ src/Topics/Physics.ElectricParticle/Physics.ElectricParticle/ViewModels/GameViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Physics.ElectricParticle.Models;
+﻿using Physics.ElectricParticle.Models;
 using Physics.ElectricParticle.Rendering;
 using Physics.Shared.UI.ViewModels;
 using Physics.Shared.UI.Views.Interactions;
@@ -12,7 +7,7 @@ namespace Physics.ElectricParticle.ViewModels
 {
 	public class GameViewModel : SimulationViewModelBase<SimulationNavigationModel>, IReceiveController<GameCanvasController>
 	{
-		public override void Prepare(SimulationNavigationModel parameter) => throw new NotImplementedException();
-		public void SetController(GameCanvasController controller) => throw new NotImplementedException();
+		public override void Prepare(SimulationNavigationModel parameter) { }
+		public void SetController(GameCanvasController controller) { }
 	}
 }
