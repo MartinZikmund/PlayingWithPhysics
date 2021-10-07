329df00e42d7bb6f7def4326dd67f35bfa5706d9
Martin Zikmund <martin@zikmund.dev>
2021-10-07 10:02:14 +0200

    Wall

--- src/Topics/Physics.LawOfConservationOfMomentum/Rendering/LawOfConservationOfMomentumCanvasController.cs
+++ src/Topics/Physics.LawOfConservationOfMomentum/Rendering/LawOfConservationOfMomentumCanvasController.cs
@@ -13,14 +13,23 @@ namespace Physics.LawOfConservationOfMomentum.Rendering
 		private readonly SKPaint _firstObjectPaint = new SKPaint()
 		{
 			IsStroke = false,
-			Color = SKColors.Red,
+			Color = SKColors.Blue,
 			IsAntialias = true,
 		};
 
 		private readonly SKPaint _secondObjectPaint = new SKPaint()
 		{
 			IsStroke = false,
-			Color = SKColors.Blue,
+			Color = SKColors.Red,
+			IsAntialias = true,
+		};
+
+
+		private readonly SKPaint _wallPaint = new SKPaint()
+		{
+			IsStroke = true,
+			StrokeWidth = ObjectSize,
+			Color = SKColors.Red,
 			IsAntialias = true,
 		};
 
@@ -59,7 +68,14 @@ namespace Physics.LawOfConservationOfMomentum.Rendering
 			var firstX = PhysicsService.GetX1((float)SimulationTime.TotalTime.TotalSeconds);
 			DrawObject(sender, args, firstX, _firstObjectPaint, 0);
 			var secondX = PhysicsService.GetX2((float)SimulationTime.TotalTime.TotalSeconds);
-			DrawObject(sender, args, secondX, _secondObjectPaint, 1);
+			if (Motion.Subtype == CollisionSubtype.V2ZeroM2BiggerThanM1)
+			{
+				DrawWall(sender, args, secondX);
+			}
+			else
+			{
+				DrawObject(sender, args, secondX, _secondObjectPaint, 1);
+			}
 		}
 
 		public override void Update(ISkiaCanvas sender)
@@ -78,6 +94,13 @@ namespace Physics.LawOfConservationOfMomentum.Rendering
 			var renderX = PadX(x * _metersToPixels);
 
 			args.Canvas.DrawCircle(objectIndex == 0 ? renderX - ObjectSize : renderX + ObjectSize, sender.ScaledSize.Height / 2, ObjectSize, paint);
+		}
+
+		private void DrawWall(ISkiaCanvas sender, SKSurface args, float x)
+		{
+			var renderX = PadX(x * _metersToPixels);
+
+			args.Canvas.DrawLine(renderX + ObjectSize / 2, 0, renderX + ObjectSize / 2, sender.ScaledSize.Height, _wallPaint);
 		}
 
 		public float PadX(float x) => HorizontalPadding + x;
