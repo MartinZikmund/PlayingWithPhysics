4423829a596ebafafc666153a01ee89f4ddfd72d
Martin Zikmund <martin@zikmund.dev>
2021-12-21 10:39:10 +0100

    Study mode for 5

--- src/Topics/Physics.RotationalInclinedPlane/Infrastructure/TopicNavigator.cs
+++ src/Topics/Physics.RotationalInclinedPlane/Infrastructure/TopicNavigator.cs
@@ -20,7 +20,7 @@ namespace Physics.RotationalInclinedPlane.Infrastructure
 
 		public bool HasAdvancedDifficulty => true;
 
-		public bool HasStudyMode => false;
+		public bool HasStudyMode => true;
 
 		public bool HasGame => false;
 
