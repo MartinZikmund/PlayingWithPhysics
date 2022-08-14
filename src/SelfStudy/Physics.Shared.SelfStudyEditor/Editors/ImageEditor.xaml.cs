4d99a092564ebb0fca49de582c02e0fccd1779a5
Martin Zikmund <martin@zikmund.dev>
2022-08-14 10:12:08 +0200

    Fix image editor

--- src/SelfStudy/Physics.Shared.SelfStudyEditor/Editors/ImageEditor.xaml.cs
+++ src/SelfStudy/Physics.Shared.SelfStudyEditor/Editors/ImageEditor.xaml.cs
@@ -24,7 +24,7 @@ namespace Physics.SelfStudy.Editor.Editors
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class ImageEditor : Page
+    public sealed partial class ImageEditor : Page, INotifyPropertyChanged
     {
         public ImageEditor()
         {
