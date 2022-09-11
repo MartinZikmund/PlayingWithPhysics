b734f258fd07afda7760042b138bc78e3c564071
Martin Zikmund <martin@zikmund.dev>
2022-09-11 22:50:41 +0200

    Add casing support to localizer

--- src/Shared/Physics.Shared/Extensions/Localize.cs
+++ src/Shared/Physics.Shared/Extensions/Localize.cs
@@ -3,10 +3,27 @@ using Windows.UI.Xaml.Markup;
 
 namespace Physics.Shared.Extensions
 {
-    public class Localize : MarkupExtension
-    {
-        public string Key { get; set; }
+	public class Localize : MarkupExtension
+	{
+		public string Key { get; set; }
 
-        protected override object ProvideValue() => Localizer.Instance.GetString(Key);
-    }
+		public bool Uppercase { get; set; } = false;
+
+		public bool Lowercase { get; set; } = false;
+
+		protected override object ProvideValue()
+		{
+			var value = Localizer.Instance.GetString(Key);
+			if (Uppercase)
+			{
+				return value.ToUpper();
+			}
+			else if (Lowercase)
+			{
+				return value.ToLower();
+			}
+
+			return value;
+		}
+	}
 }
